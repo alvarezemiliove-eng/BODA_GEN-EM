4e2a9070acb009b24ceabe992d601df7534c2f4f
alvarezemiliove-eng <alvarezemiliove-eng@users.noreply.github.com>
2026-08-09 13:18:03 -0400

    Invitacion de boda Genesis & Emilio

---
+++ config.js
@@ -0,0 +1,85 @@
+const CONFIG = {
+  pareja: "Génesis Fidealic Perdigón Paredes & Emilio José Álvarez Lopéz",
+  noviosCorto: "Génesis & Emilio",
+  fechaTexto: "10 de Octubre del 2026",
+  fechaISO: "2026-10-10T18:45:00",
+  ceremoniaLugar: "Iglesia Espíritu Santo",
+  ceremoniaHora: "6:45pm a 8:00pm",
+  ceremoniaMapa: "https://www.google.com/maps/dir/?api=1&destination=10.070612,-69.302381&travelmode=driving",
+  brindisLugar: "Villa Santa Marta",
+  brindisHora: "9:00pm a 3:00am",
+  brindisMapa: "https://www.google.com/maps/dir/?api=1&destination=10.016507,-69.302850&travelmode=driving",
+  hoteles: [
+    {
+      nombre: "Hotel Los Leones",
+      logo: "assets/logo_hotel_leones.png",
+      lujo: 2,
+      precio: "$$$",
+      coordenadas: "10.072505,-69.283363",
+      web: "https://hotellosleones.com/",
+      instagram: "https://www.instagram.com/hotellosleonesbqto/?hl=en",
+      whatsapp: "https://tr.ee/__oVbIkoQL",
+      enlace: "https://maps.app.goo.gl/zv6G3fKbEaHEJFPB9"
+    },
+    {
+      nombre: "The Place Suite",
+      logo: "assets/logo_theplace.png",
+      lujo: 2,
+      precio: "$$",
+      coordenadas: "10.063487,-69.291699",
+      instagram: "https://www.instagram.com/hoteltheplacesuites/?hl=en",
+      whatsapp: "https://api.whatsapp.com/send/?phone=584245786908&text&type=phone_number&app_absent=0&utm_source=ig",
+      enlace: "https://maps.app.goo.gl/BBdxQZe1DC79C6V36"
+    },
+    {
+      nombre: "Lidotel Barquisimeto",
+      logo: "assets/logo_lidotel.png",
+      mostrarNombre: true,
+      lujo: 5,
+      precio: "$$$$$",
+      coordenadas: "10.070626,-69.292458",
+      web: "https://lidotel.com/lidotel-barquisimeto/?gad_source=1&gad_campaignid=22322554314&gbraid=0AAAAADumJnyTcn9TDDMEwXE45R8K1wdUm&gclid=Cj0KCQjwp9vTBhCWARIsANaUrjv7uLArkYOUhy2qvruSuxmyon9DfJ3j-YUtMBaBT4a4P-2pJb2tMWoaAtlLEALw_wcB",
+      instagram: "https://www.instagram.com/lidotelbarquisimeto/?hl=en",
+      whatsapp: "https://l.instagram.com/?u=https%3A%2F%2Fapi.whatsapp.com%2Fsend%3Fphone%3D04143502651%26text%3D%25C2%25A1Buenos%2Bd%25C3%25ADas%2BLidotelBarquisimeto%2521%2BSoy%26utm_source%3Dig%26utm_medium%3Dsocial%26utm_content%3Dlink_in_bio%26fbclid%3DPAcGRvZgJleHRuA2FlbQIxMQBzcnRjBmFwcF9pZA85MzY2MTk3NDMzOTI0NTkAAacrOaXnuh5yrxT_DA-p3OMp52LGJlftruKybWOBuT9GCL4pQtqR9g0uq-d-sA_aem_HvvwCN71UoWuu18zAdTpww&e=AUCUyWykTdI3RIj_WiJ-0Tqw1emE5qoqe6B6knNDG0rHngJrlZi4aWh4aXXkB61jQWtkt4mNQUF8N16VXKKlMM4Vyc8Rzc-v6t8JuTGdWHHdD18yuE39o1gLZTlyGpRZhC6SxOI",
+      enlace: "https://www.google.com/maps/place/Lidotel+Barquisimeto/@10.0706262,-69.2924584,915m/data=!3m2!1e3!4b1!4m9!3m8!1s0x8e87677ebb723091:0x585940a576e85a1c!5m2!4m1!1i2!8m2!3d10.0706262!4d-69.2924584!16s%2Fm%2F0gyrsr_?entry=ttu"
+    }
+  ],
+  regalos: {
+    mensaje: "Con su presencia nos regalan el mejor de los momentos. Quienes quieran sumarse a los sueños que empezamos a construir, pueden hacerlo a través de:",
+    cuentas: [
+      {
+        titulo: "Transferencias en Dólares",
+        logo: "assets/logo_facebank.png",
+        filas: [
+          { etiqueta: "Beneficiary", valor: "EMILIO JOSE ALVAREZ LOPEZ", copiar: true },
+          { etiqueta: "Account Type", valor: "Current account", copiar: false },
+          { etiqueta: "Account number", valor: "21780002853", copiar: true },
+          { etiqueta: "Routing Number (ABA)", valor: "021502189", copiar: true },
+          { etiqueta: "SWIFT", valor: "FILCPR22", copiar: true },
+          { etiqueta: "Name of bank", valor: "FACEBANK International", copiar: true },
+          { etiqueta: "Bank address", valor: "17 Road 2, Suite 600, Guaynabo, Puerto Rico 00968-1787", copiar: true },
+          { etiqueta: "Client's Address", valor: "AV EL PICACHO EDIF TERRAZAS DE SAN ANTONIO PI SAN ANTONIO DE LOS ALTOS, MIRANDA, VENEZUELA, 1204", copiar: true }
+        ]
+      },
+      {
+        titulo: "Binance",
+        logo: "assets/logo_binance.png",
+        logoAncho: 96,
+        filas: [
+          { etiqueta: "Binance ID (UID)", valor: "183874567", copiar: true },
+          { etiqueta: "E-Mail", valor: "perdigongenesis@gmail.com", copiar: true }
+        ]
+      },
+      {
+        titulo: "Efectivo",
+        simbolo: "$",
+        filas: []
+      }
+    ]
+  },
+  vestimentaCodigo: "Código de vestimenta:",
+  vestimentaNivel: "Formal",
+  vestimentaNota: "De ser posible, evita usar los siguientes colores:",
+  personasDefecto: "2",
+  telefonoConfirmacion: "5210000000000"
+};
